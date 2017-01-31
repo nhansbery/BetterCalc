22940a31a5b6f3f24516687c2479d7120a3a7fb1
Nancy Howard <Nhansbery@Noahs-Macbook-Air.local>
2017-01-31 10:37:59 -0800

    2

--- Calculator.js
+++ Calculator.js
@@ -1,255 +1,269 @@
-var current_input = "";
-var memory = "0"; //always starts with zero displayed on the screen
-var operator = 0; //always starts out with no (or zero) operator being selected
-var degree = "true";
-/**
- * Displays the current input onto the calculator's screen
- */
+/** TODO:
+* 2. Bootstrap it to make it pretty!
+* 6. Challenge: Add trig functions (in radian AND degree mode)
+* 7. Extra Challenge: Add mc, m+, m-, mr butons that work!
+* 8. Super Challenge: Add ( and ) buttons that work!
+* 9. Super Duper Challenge: Add exponents (negatives too!)
+*/
+
+var currentInput = "";
+var memory = "0";
+var operator = 0;
+var degree = true;
+
 function displayCurrentInput() {
-    if (current_input == "") {
+    if(currentInput == ""){
         document.getElementById('screen').value = memory;
-    }
-    else {
-        document.getElementById('screen').value = current_input;
+    }else{
+        document.getElementById('screen').value = currentInput;
     }
 }
-/**
- * Adds the number onto the screen that user selects until it reaches 30 numbers, then restarts
- * @param {number} dig the button/value that the user selects that they want to display
- */
+
 function addDigit(dig) {
-    if ((eval(current_input) == 0) && (current_input.indexOf(".") == -1)) {
-        current_input = dig;
-    }
-    else if (current_input.length < 30) {
-        current_input = current_input + dig;
+    if ((eval(currentInput) == 0) && (currentInput.indexOf(".") == -1)) {
+        currentInput = dig;
+    } else if( currentInput.length < 30){
+        currentInput = currentInput + dig;
     }
     displayCurrentInput();
 }
-/**
- * Adds a decimal to the number on the screen each time the user selects it
- */
+
+// Adds a decimal to the current input
 function addDecimal() {
-    if (current_input.length == 0) {
+    if (currentInput.length == 0) {
         //no leading ".", use "0."
-        current_input = "0.";
-    }
-    else {
+        currentInput = "0.";
+    } else {
         // First make sure one doesn't exist
-        if (current_input.indexOf(".") == -1) {
-            current_input = current_input + ".";
+        if (currentInput.indexOf(".") == -1) {
+            currentInput = currentInput + ".";
         }
     }
     displayCurrentInput();
 }
-/**
- * Clears all of the values that are on the calculator screen
- */
+
+// Clears everything.
 function allClear() {
-    current_input = "";
-    operator = 0; //clear operator
-    memory = "0"; //clear memory
+    currentInput = "";
+    operator = 0;                //clear operator
+    memory = "0";                  //clear memory
     displayCurrentInput();
 }
-/**
- * Clears the current input value to 0
- */
+
+// Clear the current input back to 0
 function cancel() {
-    current_input = "";
+    currentInput = "";
     displayCurrentInput();
 }
-/**
- * Stores the last operator pushed for multiply, divide, add, or subtract.
- * @param {object} op all of the different operations like Addition (+), Subtraction (-), Multiplication (*), and Division (/).
- * @return {number} if there is no operator selected, returns what was there originally
- */
+
+// Stores the last operator pushed for multiply, divide, add, or subtract.
 function storeOperator(op) {
-    if (current_input == "") {
+
+    if(currentInput == ""){
         return;
     }
-    if (operator > 0) {
+
+    if(operator > 0){
         calculate();
     }
-    if (op.indexOf("*") > -1) {
-        operator = 1;
-    }; //codes for *
-    if (op.indexOf("/") > -1) {
-        operator = 2;
-    }; // slash (divide)
-    if (op.indexOf("+") > -1) {
-        operator = 3;
-    }; // sum
-    if (op.indexOf("-") > -1) {
-        operator = 4;
-    }; // difference
-    memory = current_input; //store value
-    current_input = "";
+
+    if (op.indexOf("*") > -1) { operator = 1; };       //codes for *
+    if (op.indexOf("/") > -1) { operator = 2; };       // slash (divide)
+    if (op.indexOf("+") > -1) { operator = 3; };       // sum
+    if (op.indexOf("-") > -1) { operator = 4; };       // difference
+
+    memory = currentInput;                 //store value
+    currentInput = "";
     displayCurrentInput();
 }
-/**
- * Calculates the inputted numbers using all of the operators on the calculator using the memory and what is current
- */
+
+// Calculate using operator, the memory and what is current
 function calculate() {
-    if (operator == 1) {
-        current_input = eval(memory) * eval(current_input);
-    };
-    if (operator == 2 && current_input != "0") {
-        current_input = eval(memory) / eval(current_input);
-    };
-    if (operator == 2 && current_input == "0") {
-        current_input = "ERROR"
-    };
-    if (operator == 3) {
-        current_input = eval(memory) + eval(current_input);
-    };
-    if (operator == 4) {
-        current_input = eval(memory) - eval(current_input);
-    };
-    operator = 0; //clear operator
-    memory = "0"; //clear memory
+    if (operator == 1) { currentInput = eval(memory) * eval(currentInput); };
+    if (operator == 2 && currentInput != "0"){ currentInput = eval(memory) / eval(currentInput);};
+    if (operator == 2 && currentInput == "0"){ currentInput = "ERROR" };
+    if (operator == 3) { currentInput = eval(memory) + eval(currentInput); };
+    if (operator == 4) { currentInput = eval(memory) - eval(currentInput); };
+
+    operator = 0;                //clear operator
+    memory    = "0";              //clear memory
     displayCurrentInput();
 }
-/**
- * Changes the sign of the current input if the wrong operator was selected; i.e. multiplication operator is selected when you are trying to execute an addition problem, changeSign is selected and they switch
- * @return {number} if there is no change to the sign requested, returns what was there originally
- */
+
+// Change the sign of the current input
 function changeSign() {
-    if (current_input == "") {
+
+    if(currentInput == ""){
         return;
     }
-    current_input = current_input * (-1);
+
+    currentInput = currentInput * (-1);
     displayCurrentInput();
+
 }
-/**
- * Changes the number that is on the screen currently from an integer into a percentage
- * @return {number} if no button was selected, returns what was there
- */
-function percentage() {
-    if (currentInput == "") {
+
+
+// Change the current input to a percentage
+function percentage(){
+
+    if(currentInput == ""){
         return;
     }
-    if (operator > 0) {
+
+    if(operator > 0){
         calculate();
     }
-    current_input = current_input / 100; //the percentage of a number is always that number divided by 100
+
+    currentInput = currentInput / 100;
     operator = 0;
     memory = "0";
     displayCurrentInput();
+
 }
-/**
- * Calculates the factorial of the current number when the X! button is selected
- * @return {number} if no button was selected, returns what was there
- */
+
+// Calculate the factorial of the current input
 function factorial() {
-    if (current_input == "") {
+
+    if(currentInput == ""){
         return;
     }
-    if (operator > 0) {
+
+    if(operator > 0){
         calculate();
     }
-    var fac = current_input;
-    if (fac < 0 || (fac + "").indexOf(".") != -1) {
+
+    var fac = currentInput;
+
+    if( fac < 0 ||  (fac + "").indexOf(".") != -1){
         fac = "ERROR";
-    }
-    else if (fac == 0) {
+    }else if ( fac == 0){
         fac = 1;
-    }
-    else {
-        for (i = 1; i < current_input; i++) {
+    }else{
+        for( i = 1; i < currentInput; i++){
             fac = fac * i;
         }
     }
-    current_input = fac;
+
+    currentInput = fac;
     operator = 0;
     memory = "0";
     displayCurrentInput();
+
 }
-/**
- * Squares the number (x = x*x), when the x^2 button is selected
- * @return {number} if no button was selected, returns what was there
- */
+
+// Calculate the square of the current input
 function square() {
-    if (current_input == "") {
+
+    if(currentInput == ""){
         return;
     }
-    if (operator > 0) {
+
+    if(operator > 0){
         calculate();
     }
-    curren_input = Math.pow(currentInput, 2); //the same as multiplying that number to itself
+
+    currentInput = Math.pow( currentInput, 2 );
     operator = 0;
     memory = "0";
     displayCurrentInput();
+
 }
-/**
- * Turns the current number into it's square root when the √x button is selected
- * @return {number} if no button was selected, returns what was there
- */
+
+// Calculate the square root of the current input
 function squareRoot() {
-    if (current_input == "") {
+
+    if(currentInput == ""){
         return;
     }
-    if (operator > 0) {
+
+    if(operator > 0){
         calculate();
     }
-    current_input = Math.sqrt(current_input);
+
+
+    currentInput = Math.sqrt(currentInput);
     operator = 0;
     memory = "0";
     displayCurrentInput();
+
 }
-/**
- * Calculates what the inverse of the current number is when the 1/X button is selected
- * @return {number} if no button was selected, returns what was there, but if the number is zero it will return error as there is no inverse of 0
- */
+
+// Calculate the inverse of the current input
 function inverse() {
-    if (current_input == "") {
+
+    if(currentInput == ""){
         return;
     }
-    if (current_input == 0) {
-        current_input = "ERROR";
+
+    if(currentInput == 0){
+        currentInput = "ERROR";
         operator = 0;
         memory = "0";
         displayCurrentInput();
         return;
     }
-    if (operator > 0) {
+
+    if(operator > 0){
         calculate();
     }
-    current_input = 1 / current_input;
+
+    currentInput = 1 / currentInput;
     operator = 0;
     memory = "0";
     displayCurrentInput();
+
 }
-/**
- * Takes the current number and gives the Sin of it when the sin button is selected, a trigometric function
- * @return {number} if no button was selected, returns what was there
- */
-function tigSin() {
-    if (current_input == "") {
+
+function tigSin(){
+
+    if(currentInput == ""){
         return;
     }
-    if (operator > 0) {
+
+    if(operator > 0){
         calculate();
     }
-    if (degree == true) {
-        current_input = Math.sin(current_input / );
+
+    if( degree == true ){
+        currentInput = Math.sin(currentInput * (Math.PI / 180));
+    } else {
+    currentInput = Math.sin(currentInput);
     }
+
     operator = 0;
     memory = "0";
     displayCurrentInput();
+
 }
-/**
- * Takes the current number and gives the Cos of it when the cos button is selected, a trigometric function
- * @return {number} if no button was selected, returns what was there
- */
-function tigCos() {
-    if (current_input == "") {
+
+function tigCos(){
+
+    if(currentInput == ""){
         return;
     }
-    if (operator > 0) {
+
+    if(operator > 0){
         calculate();
     }
-    current_input = Math.cos(current_input);
+
+    if( degree == true ){
+        currentInput = Math.cos(currentInput * (Math.PI / 180));
+    } else {
+    currentInput = Math.cos(currentInput);
+    }
+
     operator = 0;
     memory = "0";
     displayCurrentInput();
+
+}
+
+function tigDegree(){
+
+    if( degree == false){
+        degree = true;
+    }else{
+        degree =false;
+    }
+
 }
